a0de7df40ae85f20bee62d9e2b5207bd771acda1
demoweb <lethanh1197@gmail.com>
2024-10-13 17:12:48 +0700

    shoppingWebUpdate

--- database.sql
+++ database.sql
@@ -10,17 +10,17 @@ primary key(id)
 
 create table users(
 id int auto_increment,
-name varchar(50),
 first_name varchar(50),
 last_name varchar(50),
 user_name varchar(50),
 password varchar(20),
 email varchar(50),
-phone_numbet int(20),
+phone_number int(20),
 status int(11),
 address varchar(50),
 note varchar(255),
 role_id int(11),
+money double default(0),
 primary key(id),
 foreign key(role_id) references roles(id)
 );
@@ -28,13 +28,18 @@ foreign key(role_id) references roles(id)
 create table product_types(
 id int auto_increment,
 name varchar(100),
+add_time datetime,
+note varchar(255),
 primary key(id)
 );
 
 create table reviews(
 id int auto_increment,
 review varchar(255),
-primary key(id)
+rate double,
+user_id int,
+primary key(id),
+foreign key(user_id) references users(id)
 );
 
 create table products(
@@ -42,7 +47,7 @@ id int auto_increment,
 color varchar(50),
 description varchar(255),
 price double,
-discount double,
+price_discount double,
 size varchar(50),
 img varchar(255),
 status varchar(255),
